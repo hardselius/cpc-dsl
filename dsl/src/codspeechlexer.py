3816240997238308db73b4772695ae2c8c47ada2
Martin Hardselius <martin.hardselius@gmail.com>
2012-02-28 15:04:18 +0100

    identifiers

--- dsl/src/codspeechlexer.py
+++ dsl/src/codspeechlexer.py
@@ -9,6 +9,7 @@ import sys
 sys.path.insert(0,"../..")
 
 import ply.lex as lex
+from ply.lex import TOKEN
 
 # Reserved words
 reserved = {
@@ -54,11 +55,22 @@ tokens = (
     'COMMA', 'PERIOD', 'SEMI', 'COLON', 'DOUBLECOLON'
     )
 
+# Complex REs
+digit = r'([0-9])'
+lowercase = r'([a-z])'
+uppercase = r'([A-Z])'
+nondigit = r'([_A-Za-z])'
+identifier = r'(' + lowercase + r'(' digit + r'|' + nondigit + r')*)'
+
+
 # Ignored characters
 t_ignore = ' \t\x0c'
 
 # Literals
-
+@TOKEN(identifier)
+def t_ID:
+    t.type = reserved.get(t.value,"ID")
+    return t
 
 # Assignment operators
 t_EQUALS       = r'='
@@ -81,3 +93,7 @@ t_PERIOD       = r'\.'
 r_SEMI         = r';'
 r_COLON        = r':'
 r_DOUBLECOLON  = r'::'
+
+def t_newline(t):
+    r'\n'
+    t.lexer.lineno += len(t.value)
