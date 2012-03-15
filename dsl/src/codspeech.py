0da75d9b0ab518d6f415e98c859f5c93a2c7df60
Martin Hardselius <martin.hardselius@gmail.com>
2012-03-15 15:01:51 +0100

    added typechecking

--- dsl/src/codspeech.py
+++ dsl/src/codspeech.py
@@ -4,8 +4,9 @@ import sys
 sys.path.insert(0,"../..")
 
 import os
-import codspeechlexer  as cslex
-import codspeechparser as csparse
+import codspeechlexer       as cslex
+import codspeechparser      as csparse
+import codspeechtypechecker as cstype
 
 lexer  = cslex.lex.lex(module=cslex)
 parser = csparse.yacc.yacc(module=csparse)
@@ -103,14 +104,16 @@ def main(*args):
         '--xml',
         action   = 'store',
         dest     = 'xml',
-        default  = 'lol',
+        default  = '',
         metavar  = 'FILE.xml',
         help     = 'Generate an xml representation of the program.'
     )
     cmdparser.add_option_group(compile_opts)
 
     opts, args = cmdparser.parse_args(*args)
-    program = ''
+    program    = None # Contents of read file
+    ast        = None # Abstract Syntax Tree
+    ctx        = None # Context after typechecking
     
     if len(args) != 1:
         cmdparser.print_help()
@@ -126,18 +129,24 @@ def main(*args):
     if opts.lex:
         print_header('Tokens',args[0])
         get_tokens(program)
-        print '-' * 68 + '\n'
         
     if opts.ast:
         print_header('Abstract Syntax Tree',args[0])
-        print get_ast(program)
-        print '-' * 68 + '\n'
-        
+        ast = get_ast(program)
+        print ast
+
+    if opts.typecheck:
+        if not ast: ast = get_ast(program)
+        ctx = cstype.typecheck(ast)
+        print_header('Typecheck',args[0])
+        if not ctx:
+            print "Typechecking failed"
+        else:
+            print ctx
+            print "Typechecking passed. Everything correct."
         
 def print_header(s,f):
-    print '-' * 68
-    print "\t{1}: {0}".format(s,f)
-    print '-' * 68 + '\n'
+    print "\n\t{1} - {0}:\n".format(s,f)
 
         
 if __name__ == '__main__':
