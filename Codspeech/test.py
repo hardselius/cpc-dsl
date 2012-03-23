8833e5ff9bd94f3b489bc0976fdba9d315b892cf
Martin Hardselius <martin.hardselius@gmail.com>
2012-03-23 17:29:34 +0100

    test file

--- Codspeech/test.py
+++ Codspeech/test.py
@@ -1,7 +1,20 @@
-import codspeech.parser.csparser  as parser
-import codspeech.parser.cslexer   as lexer
-import codspeech.parser.plyparser
 
-import codspeech.ast.csast
-import codspeech.typechecker.cstypechecker
-import codspeech.xml.cstoxml
+
+import codspeech.parser.csparser  as parser
+import codspeech.ast.csast as csast
+
+
+# --------------------------------------------------------------
+# test functions
+# --------------------------------------------------------------
+def test(filepath):
+    p = parser.CodspeechParser()
+    try:
+        buffer = open(filepath)
+    except IOError, e:
+        raise e
+    else:
+        prog = buffer.read()
+        return p.parse(prog, filepath)
+
+t = test('examples/example3.cod')
