38f689cc020f28e544bef372ca6462a580bcb9df
Martin Hardselius <martin.hardselius@gmail.com>
2012-03-23 17:29:09 +0100

    new ast

--- Codspeech/codspeech/ast/csast.py
+++ Codspeech/codspeech/ast/csast.py
@@ -1,304 +1,397 @@
 # ----------------------------------------------------------------------
 # Codspeech/codspeech/ast: csast.py
 #
-# A parser for Codspeech
+# AST Node classes for Codspeech
 # ----------------------------------------------------------------------
 
+import sys
 
-indent = ""
 
-def strNewLine(x):
-        if x == []:
-            return ""
+class Node(object):
+    """ Abstract base class for AST nodes.
+    """
+    def children(self):
+        """ A sequence of all children that are Nodes
+        """
+        pass
+
+    def show(self, buf=sys.stdout, offset=0, attrnames=False, nodenames=False, showcoord=False, _my_node_name=None):
+        """ Pretty print the Node and all its attributes and
+            children (recursively) to a buffer.
+
+            buf:
+                Open IO buffer into which the Node is printed.
+
+            offset:
+                Initial offset (amount of leading spaces)
+
+            attrnames:
+                True if you want to see the attribute names in
+                name=value pairs. False to only see the values.
+
+            nodenames:
+                True if you want to see the actual node names
+                within their parents.
+
+            showcoord:
+                Do you want the coordinates of each Node to be
+                displayed.
+        """
+        lead = ' ' * offset
+        if nodenames and _my_node_name is not None:
+            buf.write(lead + self.__class__.__name__+ ' <' + _my_node_name + '>: ')
         else:
-            y = x.pop(0)
-            return "\n" + indent + str(y) + strNewLine(x)
+            buf.write(lead + self.__class__.__name__+ ': ')
 
-class Program(object):
-    def __init__(self,i,c,t):
-        self.imports    = i
-        self.components = c
-	self.newtypes   = t
+        if self.attr_names:
+            if attrnames:
+                nvlist = [(n, getattr(self,n)) for n in self.attr_names]
+                attrstr = ', '.join('%s=%s' % nv for nv in nvlist)
+            else:
+                vlist = [getattr(self, n) for n in self.attr_names]
+                attrstr = ', '.join('%s' % v for v in vlist)
+            buf.write(attrstr)
 
-    def __str__(self):
-        global indent
-        indent = "    "
-        s = "Program:" + indent + strNewLine(self.imports) + \
-            "\n" + indent + strNewLine(self.newtypes) + \
-            "\n" + indent + strNewLine(self.components)
-        indent = ""
-        return s
+        if showcoord:
+            buf.write(' (at %s)' % self.coord)
+        buf.write('\n')
 
-
-class Import(object):
-    def __init__(self,p):
-        self.path = p
-
-    def __str__(self):
-        global indent
-        return "Import: " + str(self.path)
+        for (child_name, child) in self.children():
+            child.show(
+                buf,
+                offset=offset + 2,
+                attrnames=attrnames,
+                nodenames=nodenames,
+                showcoord=showcoord,
+                _my_node_name=child_name)
 
 
-class NewType(object):
-    def __init__(self,id,b,d = None):
-        self.ident = id
-	self.body  = b
-	self.doc   = d
+class NodeVisitor(object):
+    """ A base NodeVisitor class for visiting c_ast nodes.
+        Subclass it and define your own visit_XXX methods, where
+        XXX is the class name you want to visit with these
+        methods.
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "NewType: " + str(self.ident) + \
-            indent + strNewLine(self.body)
-        indent = indent[:len(indent)-4]
-        return s
+        For example:
 
+        class ConstantVisitor(NodeVisitor):
+            def __init__(self):
+                self.values = []
 
-class NewTypeObject(object):
-    def __init__(self,id,t):
-        self.ident = id
-	self.type  = t
+            def visit_Constant(self, node):
+                self.values.append(node.value)
 
-    def __str__(self):
-        global indent
-        return "NWObject: " + str(self.ident) + ", " + str(self.type)
+        Creates a list of values of all the constant nodes
+        encountered below the given node. To use it:
 
+        cv = ConstantVisitor()
+        cv.visit(node)
 
-class Import(object):
-    def __init__(self,p):
-        self.path = p
+        Notes:
 
-    def __str__(self):
-        global indent
-        return "Import: " + str(self.path)
+        *   generic_visit() will be called for AST nodes for which
+            no visit_XXX method was defined.
+        *   The children of nodes for which a visit_XXX was
+            defined will not be visited - if you need this, call
+            generic_visit() on the node.
+            You can use:
+                NodeVisitor.generic_visit(self, node)
+        *   Modeled after Python's own AST visiting facilities
+            (the ast module of Python 3.0)
+    """
+    def visit(self, node):
+        """ Visit a node.
+        """
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
 
-
-class Component(object):
-    def __init__(self,h,b):
-        self.header = h
-        self.body   = b
-
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Component:\n" + indent + str(self.header) + \
-            "\n" + indent + str(self.body)
-        indent = indent[:len(indent)-4]
-	return s
-
-
-class Header(object):
-    def __init__(self,id,i,o,d = None):
-        self.ident   = id
-        self.doc     = d
-        self.inputs  = i
-        self.outputs = o
-
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Header:\n" + indent + str(self.ident) + \
-            "\n" + indent + str(self.doc) + \
-            "\n" + indent + strNewLine(self.inputs) + \
-            "\n" + indent + strNewLine(self.outputs)
-        indent = indent[:len(indent)-4]
-	return s
+    def generic_visit(self, node):
+        """ Called if no explicit visitor function exists for a
+            node. Implements preorder visiting of the node.
+        """
+        for c_name, c in node.children():
+            self.visit(c)
 
 
-class InParameter(object):
-    def __init__(self,id,t,d = None,de = None,opt = False):
-        self.ident    = id
-        self.type     = t
-        self.doc      = d
-        self.optional = opt
-        self.default  = de
+class Program(Node):
+    def __init__(self, imports, components, newtypes, coord=None):
+        self.imports = imports
+        self.components = components
+        self.newtypes = newtypes
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "InParameter:\n" + indent + str(self.ident) + \
-            "\n" + indent + str(self.type) + \
-            "\n" + indent + str(self.optional) + \
-            "\n" + indent + str(self.default) + \
-            "\n" + indent + str(self.doc)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        for i, child in enumerate(self.imports or []):
+            nodelist.append(("imports[%d]" % i, child))
+        for i, child in enumerate(self.components or []):
+            nodelist.append(("components[%d]" % i, child))
+        for i, child in enumerate(self.newtypes or []):
+            nodelist.append(("newtypes[%d]" % i, child))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class OutParameter(object):
-    def __init__(self,id,t,d = None,de = None):
-        self.ident    = id
-        self.type     = t
-        self.doc      = d
-        self.default  = de
+class Import(Node):
+    def __init__(self, path, coord=None):
+        self.path = path
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "OutParameter:\n" + indent + str(self.ident) + \
-            "\n" + indent + str(self.type) + \
-            "\n" + indent + str(self.default) + \
-            "\n" + indent + str(self.doc)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
 
+    attr_names = ('path',)
 
-class Network(object):
-    def __init__(self,b,contr = None):
-        self.body       = b
-        self.controller = contr
+class Component(Node):
+    def __init__(self, header, body, coord=None):
+        self.header = header
+        self.body = body
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Network:\n" + indent + str(self.controller) + \
-            indent + strNewLine(self.body)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.header is not None: nodelist.append(("header", self.header))
+        if self.body is not None: nodelist.append(("body", self.body))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Controller(object):
-    def __init__(self,t,id):
-        self.type  = t
-        self.ident = id
+class Header(Node):
+    def __init__(self, ident, doc, inputs, outputs, coord=None):
+        self.ident = ident
+        self.doc = doc
+        self.inputs = inputs
+        self.outputs = outputs
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Controller: " + str(self.type) + \
-            "\n" + indent + str(self.ident)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        if self.doc is not None: nodelist.append(("doc", self.doc))
+        for i, child in enumerate(self.inputs or []):
+            nodelist.append(("inputs[%d]" % i, child))
+        for i, child in enumerate(self.outputs or []):
+            nodelist.append(("outputs[%d]" % i, child))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Atom(object):
-    def __init__(self,t,opts):
-        self.type    = t
-        self.options = opts
+class InParameter(Node):
+    def __init__(self, ident, type, doc, optional=False, default=None, coord=None):
+        self.ident = ident
+        self.type = type
+        self.doc = doc
+        self.optional = Optional(optional)
+        self.default = default
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Atom: " + str(self.type) + \
-            "\n" + indent + strNewLine(self.options)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        if self.type is not None: nodelist.append(("type", self.type))
+        if self.doc is not None: nodelist.append(("doc", self.doc))
+        if self.optional is not None: nodelist.append(("optional", self.optional))
+        if self.default is not None: nodelist.append(("default", self.default))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class AtomOption(object):
-    def __init__(self,opt,val):
-        self.option = opt
-        self.value  = val
+class OutParameter(Node):
+    def __init__(self, ident, type, doc, default=None, coord=None):
+        self.ident = ident
+        self.type = type
+        self.doc = doc
+        self.default = default
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "AtomOption:\n" + indent + str(self.option) + \
-               "\n" + indent + str(self.value)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        if self.type is not None: nodelist.append(("type", self.type))
+        if self.doc is not None: nodelist.append(("doc", self.doc))
+        if self.default is not None: nodelist.append(("default", self.default))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Connection(object):
-    def __init__(self,l,r):
-        self.left  = l
-        self.right = r
+class Network(Node):
+    def __init__(self, body, controller=None, coord=None):
+        self.body = body
+        self.controller = controller
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Connection:\n" + indent + str(self.left) + \
-            "\n" + indent + str(self.right)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.controller is not None: nodelist.append(("controller", self.controller))
+        for i, child in enumerate(self.body or []):
+            nodelist.append(("body[%d]" % i, child))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Assignment(object):
-    def __init__(self,id,comp):
-        self.ident     = id
-        self.component = comp
+class Atom(Node):
+    def __init__(self, type, options, coord=None):
+        self.type = type
+        self.options = options
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Assignment:\n" + indent + str(self.ident) + \
-            "\n" + indent + str(self.component)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.type is not None: nodelist.append(("type", self.type))
+        for i, child in enumerate(self.options or []):
+            nodelist.append(("options[%d]" % i, child))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class ComponentStmt(object):
-    def __init__(self,id,i):
-        self.ident  = id
-        self.inputs = i
+class AtomType(Node):
+    def __init__(self, type, coord=None):
+        self.type = type
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "ComponentStmt:\n" + indent + str(self.ident) + \
-            indent + strNewLine(self.inputs)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
 
+    attr_names = ('type',)
 
-class Const(object):
-    def __init__(self,t,v):
-        self.type  = t
-        self.value = v
+class AtomOption(Node):
+    def __init__(self, option, value, coord=None):
+        self.option = option
+        self.value = value
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        return "Const: " + str(self.type) + " " + str(self.value)
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
 
+    attr_names = ('option','value')
 
-class This(object):
-    def __init__(self,id,io):
-        self.ident     = id
-        self.io        = io
+class Connection(Node):
+    def __init__(self, left, right, coord=None):
+        self.left = left
+        self.right = right
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "This: " + str(self.io) + " " + str(self.ident)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.left is not None: nodelist.append(("left", self.left))
+        if self.right is not None: nodelist.append(("right", self.right))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Other(object):
-    def __init__(self,id,io,comp):
-        self.ident     = id
-        self.io        = io
-        self.component = comp
+class Assignment(Node):
+    def __init__(self, ident, comp, coord=None):
+        self.ident = ident
+        self.comp = comp
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Other: " + str(self.component) + \
-            " " + str(self.io) + " " + str(self.ident)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        if self.comp is not None: nodelist.append(("comp", self.comp))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Comp(object):
-    def __init__(self,id,io,comp):
-        self.ident     = id
-        self.io        = io
-        self.component = comp
+class ComponentStmt(Node):
+    def __init__(self, ident, inputs, coord=None):
+        self.ident = ident
+        self.inputs = inputs
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        indent += "    "
-        s = "Comp: " + str(self.io) + " " + str(self.ident) + \
-            "\n" + indent + str(self.component)
-        indent = indent[:len(indent)-4]
-	return s
+    def children(self):
+        nodelist = []
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        for i, child in enumerate(self.inputs or []):
+            nodelist.append(("inputs[%d]" % i, child))
+        return tuple(nodelist)
 
+    attr_names = ()
 
-class Ident(object):
-    def __init__(self,n,p = '?'):
-        self.name = n
-        self.pos  = p
+class Const(Node):
+    def __init__(self, type, value, coord=None):
+        self.type = type
+        self.value = value
+        self.coord = coord
 
-    def __str__(self):
-        global indent
-        return "Ident: " + str(self.name) + " " + str(self.pos)
+    def children(self):
+        nodelist = []
+        if self.type is not None: nodelist.append(("type", self.type))
+        return tuple(nodelist)
+
+    attr_names = ('value',)
+
+class ParamRef(Node):
+    def __init__(self, comp, io, ident, coord=None):
+        self.comp = comp
+        self.io = io
+        self.ident = ident
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        if self.comp is not None: nodelist.append(("comp", self.comp))
+        if self.io is not None: nodelist.append(("io", self.io))
+        if self.ident is not None: nodelist.append(("ident", self.ident))
+        return tuple(nodelist)
+
+    attr_names = ()
+
+class Ident(Node):
+    def __init__(self, name, coord=None):
+        self.name = name
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('name',)
+
+class Type(Node):
+    def __init__(self, type, coord=None):
+        self.type = type
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('type',)
+
+class DocString(Node):
+    def __init__(self, doc, coord=None):
+        self.doc = doc
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('doc',)
+
+class Optional(Node):
+    def __init__(self, bool, coord=None):
+        self.bool = bool
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('bool',)
+
+class Ref(Node):
+    def __init__(self, ref, coord=None):
+        self.ref = ref
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('ref',)
