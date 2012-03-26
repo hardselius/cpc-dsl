a43cfdb6756019840405faf96d4ecf90e510b1cc
Martin Hardselius <martin.hardselius@gmail.com>
2012-03-26 15:07:36 +0200

    blah

--- Codspeech/codspeech/typechecker/cstypechecker_visitor.py
+++ Codspeech/codspeech/typechecker/cstypechecker_visitor.py
@@ -21,6 +21,7 @@ def _error_ref(coord,msg):
 class TypeChecker(csast.NodeVisitor):
     def __init__(self, debug=False):
         self.env = [{}]
+        self.currentComp = ''
         self.debug = debug
         if self.debug:
             print '\nDebug: ON'
@@ -33,9 +34,11 @@ class TypeChecker(csast.NodeVisitor):
 
 
     def _getEnv(self):
+        """Returns the last record of the environment."""
         return self.env[-1]
 
     def _resetEnv(self):
+        """Resest the environment."""
         self.env = [{}]
 
     def _pop(self):
@@ -72,11 +75,15 @@ class TypeChecker(csast.NodeVisitor):
             print 'Visiting:', node.__class__.__name__, msg
         else:
             pass
-
-    def _getType(self, node):
-        pass
+                    
 
     def visit_Program(self, node):
+        """
+        Visit Program-nodes.
+        *   Add all Component names and their types to the environment.
+        *   [TODO] Add newtypes
+        *   [TODO] Add imports?
+        """
         self._debug(node, '')
         for comp in node.components:
             self._add(
@@ -85,45 +92,102 @@ class TypeChecker(csast.NodeVisitor):
                   'out' : comp.header.outputs })
         for cname, c in node.children():
             self.visit(c)
+
+
+#    def visit_Component(self, node):
+#        self._debug(node, '')
+#        self._put()
+#        for cname, c in node.children():
+#            self.visit(c)
+#        print self._getEnv()
+#        self.pop()
+#
+#    def visit_Header(self, node):
+#        self._debug(node, '')
+#        self.component = node.ident.name
+#        
+#
+#    def visit_InParameter(self, node):
         
+
+    
+
+
     def visit_Component(self, node):
         self._debug(node, node.header.ident.name)
         self._put()
         self._add(csast.Ident('in'),{})
         self._add(csast.Ident('out'),{})
-        self.visit(node.header)
+        for cname, c in node.children():
+            self.visit(c)
+        print self._getEnv()
         self._pop()
 
+        
     def visit_Header(self, node):
         self._debug(node,'')
+        self.currentComp = node.ident.name
         for cname, c in node.children():
             self.visit(c)
 
+            
     def visit_InParameter(self, node):
         self._debug(node,node.ident.name)
         self._addParameter(node, 'in')
 
+        
     def visit_OutParameter(self, node):
         self._debug(node,node.ident.name)
         self._addParameter(node, 'out')
 
+        
     def visit_Network(self, node):
         self._debug(node,'')
         for cname, c in node.children():
             self.visit(c)
 
+            
     def visit_Controller(self, node):
         self._debug(node,'')
         pass
 
+        
     def visit_Connection(self, node):
         self._debug(node,'')
         pass
 
+        
     def visit_Assignment(self, node):
         self._debug(node,'')
         pass
 
+        
     def visit_ComponentStmt(self, node):
         self._debug(node,'')
         pass
+
+        
+#    def _inferType(self, node):
+#        if type(node) == csast.Const:
+#            return node.type.type
+#        elif type(node) == csast.Ident:
+#            return self._getEnv()[node.name]
+#            elif type(node) = csast.
+#        elif type(node) == csast.ParamRef:
+#            io = node.io.ref
+#            if type(node.comp) == csast.Ident:
+#                return self._typeParams(
+#                    node.comp.name,
+#                    self._getEnv()[]
+#                )
+#            elif type(node.comp) == csast.ComponentStmt:
+#                pass
+#            else:
+#                return self._lookupParamType(
+#                    node.ident.name,
+#                    self._getEnv()[self.currentComp][io])
+
+    def _lookupParamType(self, id, params):
+        for p in params:
+            if p.ident.name == id: return p.type.type
+        
